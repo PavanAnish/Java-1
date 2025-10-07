47a092f5bfae4a9196819b066a60318ee068b2f2
PavanvAnish <pavanpavan6167@gmail.com>
2025-10-07 23:25:13 +0530

    Update DescArray.java

--- array/DescArray.java
+++ array/DescArray.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+
 class DescArray{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
@@ -26,4 +28,5 @@ for(int i=0;i<n;i++){
 System.out.print(arr[i]+" ");}
 System.out.print("]");
 }
+
 }
